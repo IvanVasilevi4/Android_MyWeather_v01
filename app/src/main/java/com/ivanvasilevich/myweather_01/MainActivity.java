2377b63e5555a8b99c9c95b6dfc6b3a486249ceb
Ivan Vasilevich <l2eurotik@gmail.com>
2019-12-12 01:51:45 +0300

    v0.1

--- app/src/main/java/com/ivanvasilevich/myweather_01/MainActivity.java
+++ app/src/main/java/com/ivanvasilevich/myweather_01/MainActivity.java
@@ -1,13 +1,6 @@
 package com.ivanvasilevich.myweather_01;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Switch;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +19,4 @@ public class MainActivity extends AppCompatActivity {
             ll.setBackgroundColor(getResources().getColor(R.color.colorLightBlue));
         }
     }
-}
+}*/
